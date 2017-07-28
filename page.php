8116fe21103279e3a64e86731780b065737cf0fe
danielgbullido <daniel.gbullido@gmail.com>
2017-07-28 10:04:29 +0200

    add page

--- page.php
+++ page.php
@@ -13,26 +13,33 @@
  */
 
 get_header(); ?>
+<div class="row">
+    <?php
+    if (is_home() && !is_front_page()) : ?>
+        <div class="page-header">
+            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+        </div>
+    <?php endif; ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+    <div class="container-fluid">
+        <section id="primary" class="site-main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+            <?php
+            while (have_posts()) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+                get_template_part('template-parts/content', 'page');
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()) :
+                    comments_template();
+                endif;
 
-			endwhile; // End of the loop.
-			?>
+            endwhile; // End of the loop.
+            ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </section><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+        <?php get_sidebar(); ?>
+    </div>
+    <?php get_footer(); ?>
+</div>
