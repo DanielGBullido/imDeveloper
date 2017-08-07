a2e66bc0aeb09ec7c1e8c1c5b81714362ffb26f9
danielgbullido <daniel.gbullido@gmail.com>
2017-08-07 18:52:31 +0200

    Add custom meta box for proyects

--- functions.php
+++ functions.php
@@ -168,3 +168,5 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+require get_template_directory() . '/inc/proyects.php';
+
